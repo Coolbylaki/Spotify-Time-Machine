bc8306d0f04797596220f37a156bf45a5b2afb4d
Coolbylaki <lakisd98@gmail.com>
2022-05-01 18:56:41 +0200

    Made a list of all 100 spotify uris

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ BILLBOARD_ENDPOINT = "https://www.billboard.com/charts/hot-100"
 # date_choice = input("Which year do you want to travel to?"
 #                     "Type the date in this format YYYY-MM-DD: ")
 date_choice = "2020-02-07"
+date_year = date_choice[:4]
 
 response = requests.get(f"{BILLBOARD_ENDPOINT}/{date_choice}/")
 
@@ -20,7 +21,7 @@ billboard_website = response.text
 soup = BeautifulSoup(billboard_website, "html.parser")
 songs = soup.select(selector="li h3#title-of-a-story")
 
-top_100 = [song.getText().strip() for song in songs]
+top_100_songs = [song.getText().strip() for song in songs]
 
 OAuth = SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID,
                      client_secret=SPOTIFY_CLIENT_SECRET,
@@ -30,5 +31,13 @@ OAuth = SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID,
                      cache_path=".cache")
 
 sp = spotipy.Spotify(auth_manager=OAuth)
-
 user_id = sp.current_user()["id"]
+
+song_uris = []
+for song in top_100_songs:
+    result = sp.search(q=f"track:{song} year:{date_year}", type="track")
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
