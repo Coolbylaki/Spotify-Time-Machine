45e39f35e46e335b492ea1625bd8b3edbfbbb012
Coolbylaki <lakisd98@gmail.com>
2022-05-01 18:38:47 +0200

    Authenticated with Spotify

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
+import os
+import spotipy
+from spotipy import SpotifyOAuth
 
-Billboard_endpoint = "https://www.billboard.com/charts/hot-100"
+SPOTIFY_REDIRECT_URI = "https://example.com"
+SPOTIFY_CLIENT_ID = os.environ.get("Spotify_id")
+SPOTIFY_CLIENT_SECRET = os.environ.get("Spotify_secret")
+BILLBOARD_ENDPOINT = "https://www.billboard.com/charts/hot-100"
 
-date_choice = input("Which year do you want to travel to?"
-                    "Type the date in this format YYYY-MM-DD: ")
+# date_choice = input("Which year do you want to travel to?"
+#                     "Type the date in this format YYYY-MM-DD: ")
+date_choice = "2020-02-07"
 
-response = requests.get(f"{Billboard_endpoint}/{date_choice}/")
+response = requests.get(f"{BILLBOARD_ENDPOINT}/{date_choice}/")
 
 billboard_website = response.text
 
@@ -14,3 +21,14 @@ soup = BeautifulSoup(billboard_website, "html.parser")
 songs = soup.select(selector="li h3#title-of-a-story")
 
 top_100 = [song.getText().strip() for song in songs]
+
+OAuth = SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID,
+                     client_secret=SPOTIFY_CLIENT_SECRET,
+                     redirect_uri=SPOTIFY_REDIRECT_URI,
+                     scope="playlist-modify-private",
+                     show_dialog=True,
+                     cache_path=".cache")
+
+sp = spotipy.Spotify(auth_manager=OAuth)
+
+user_id = sp.current_user()["id"]
